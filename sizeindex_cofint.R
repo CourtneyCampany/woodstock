f37e1d4e8197f37bee03938ada8d1d2c5a51b47b
Courtney Campany <courtneycampany@gmail.com>
2017-02-09 09:46:15 +1100

    started CI script of si and volume

---
+++ sizeindex_cofint.R
@@ -0,0 +1,46 @@
+
+# read data ---------------------------------------------------------------
+
+si_clim <- read.csv("data/si_climate.csv")
+standard <- read.csv("data/container_assessment.csv")
+
+
+# simple model and predict CI ---------------------------------------------
+
+si_mod <- lm(logSI ~ logvol, data=si_clim)
+
+##seqeunce of volumes (log10) to predict from
+volseq <- seq(min(si_clim$logvol), max(si_clim$logvol), length=501)
+
+si_pred <- predict(si_mod, newdata = data.frame(logvol = volseq), se.fit=TRUE)
+
+##ci
+si_upr <- si_pred$fit + (2*si_pred$se.fit)
+si_lwr <- si_pred$fit - (2*si_pred$se.fit)
+
+
+# plot CI -----------------------------------------------------------------
+silab <- expression(Size~index~range~~(calliper~x~height))
+si_range <- si_clim[si_clim$volume >= 18,]
+
+windows(7,7)
+
+par(mar=c(5,5,2,1),cex.axis=1, cex.lab=1.25,las=0,mgp=c(3,1,0))
+
+plot(logSI ~ logvol, data=si_range, xlab="Container volume (L)", 
+     ylab=silab, xlim=c(1,3.7), ylim=c(0.3,3.7),
+     axes=FALSE, type='n')
+
+magicaxis::magaxis(side=c(1,2), unlog=c(1,2), frame.plot=FALSE)
+
+#add assessment
+lines(log10(min_size_index[1:36])~log10(container_volume[1:36]), data=standard,lwd=2)
+lines(log10(max_size_index[1:36])~log10(container_volume[1:36]), data=standard,lwd=2)
+
+lines(volseq, si_upr, lty=2, lwd=2,col="royalblue")
+lines(volseq, si_lwr, lty=2, lwd=2,col="royalblue")
+lines(volseq, si_pred$fit, lty=1, lwd=2,col="royalblue")
+
+legend("topleft", "AS2303 Size Index Range" ,lty=1, lwd=2,bty='n', inset=.01)
+
+box()
