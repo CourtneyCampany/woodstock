b725f49951e6f653ae9b438287b68d4d658dab01
Courtney Campany <courtneycampany@gmail.com>
2017-04-04 16:27:52 +1000

    organized SI script stats

--- master_scripts/stats_si.R
+++ master_scripts/stats_si.R
@@ -17,6 +17,7 @@ shape <- read.csv("data/tree_shape.csv")
 shape_range <- shape[shape$volume>=18,]
   shape_range <- transform(shape_range,ID = paste(nursery, batch_id, sep="_"))
 
+  
 # format data and merge data -------------------------------------------------------------
 si_agg <- summaryBy(. ~ ID, FUN=mean, keep.names=TRUE,
                  id= ~ species+origin+leaf_type+nursery+climate_region,
@@ -37,13 +38,26 @@ tree_stats <- tree_dat[-rowstodelete,c(1:19,44:50)]
 write.csv(tree_stats, "data/tree_stats.csv", row.names = FALSE)
 
 
+
 # Size Index stats ------------------------------------------------------
+nullmod <- lmer(logSI ~ 1 + (1|species), data=tree_stats)  # 19% of the variance explained at species level
+nullmod2 <- lmer(logSI ~ 1 + (1|nursery/species), data=tree_stats) # dp I add variance terms?
+
+###should nursery be included as a fixed effect to test differences in nursery practices (in the absense of fert/irrig data)?
+###log data or raw?
+###stepwise selection for all fixed effects?
+###interaction terms
+###variance paritioning on best model
+
 lme0 <- lmer(logSI ~ logvol + (1|nursery/species), data=tree_stats)
+summary(lme0)
 visreg(lme0)
-coef(lme0)
 
 lme1 <- lmer(logSI ~ logvol + (1|species), data=tree_stats)
 visreg(lme1)
+
+
+# individual fixed effects ------------------------------------------------
 
 #mean annual temp
 lme2 <- lmer(logSI ~ logvol*MAT + (1|nursery/species), data=tree_stats)
@@ -56,6 +70,7 @@ visreg(lme2a, "logvol", by="MAP", overlay=TRUE)
 #climate region
 lme3 <- lmer(logSI ~ logvol*climate_region + (1|nursery/species), data=tree_stats)
 visreg(lme3, "logvol", by="climate_region", overlay=T)
+contrasts(tree_stats$climate_region)
 
 #deciduous or evergreen
 lme4 <- lmer(logSI ~ logvol*leaf_type + (1|nursery/species), data=tree_stats)
@@ -72,6 +87,7 @@ visreg(lme6, "logvol", by="crown_spread", overlay=TRUE)
 #branchiness
 lme7 <- lmer(logSI ~ logvol*branchper30 + (1|nursery/species), data=tree_stats)
 visreg(lme7, "logvol", by="branchper30", overlay=TRUE)
+
 
 
 # full model --------------------------------------------------------------
@@ -101,7 +117,8 @@ barplot(sort(r$nursery[[1]]),
         names.arg=rownames(r$nursery))
 
 
-# partition variance ------------------------------------------------------
+
+# R2 ------------------------------------------------------
 library(MuMIn)
 
 #nursery and species as random effects
@@ -136,23 +153,25 @@ Anova(lme7)
   ##appears to also be affect of branchiness (less branches bigger in small containers)
 
 
+
 # questions to address ----------------------------------------------------
 
 #1) do deciduous trees have bigger crowns, during early development
-#2) are deciduous less branchy then evergreen during early development
-#3) where are decidous bigger during production
+
 leafmod <- lmer(crown_spread ~ leaf_type + (1|nursery/species), data=tree_stats)
 visreg(leafmod, "leaf_type", overlay=TRUE)
 summary(leafmod)
 Anova(leafmod)
 #1: no for all data, yes if you define lower volumes (<=400)
 
+#2) are deciduous less branchy then evergreen during early development
 leafmod2 <- lmer(branchper30 ~ leaf_type + (1|nursery/species), data=tree_stats)
 visreg(leafmod2, "leaf_type", overlay=TRUE)
 summary(leafmod)
 Anova(leafmod2)
 #2: yes 
 
+#3) when are decidous bigger during production
 leafmod3 <- lmer(logSI ~ logvol*leaf_type + (1|nursery/species), data=tree_stats[tree_stats$volume >300,])
 visreg(leafmod3, "logvol", by="leaf_type", overlay=TRUE)
 Anova(leafmod3)
